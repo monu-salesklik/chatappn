f67c1ca2f0e05d5fe9a5c5146b62d17284760a3c
Monu <129953445+monu-salesklik@users.noreply.github.com>
2024-06-17 16:39:41 +0530

    "context updated"

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from pydantic import BaseModel
 
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key="AIzaSyB6kfcGhxQ64by7KEH5sOOOkGxVowoV3xA")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 app = FastAPI()
@@ -49,7 +49,7 @@ def get_conversational_chain():
 
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    provided context just try to provide the answer from yourself if you can\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
 
